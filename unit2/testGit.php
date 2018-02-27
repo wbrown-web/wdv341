c17ff888b5a2a44ba7ac906aff6d9b3b9a2ebb28
Will Brown <wbrown.web@gmail.com>
2018-02-26 22:43:35 -0600

    Updated folder/files with new lines of code.

--- unit2/testGit.php
+++ unit2/testGit.php
@@ -5,6 +5,8 @@
 </head>
   <?php $yourName = "Will Brown"; ?>
   <?php echo "<h2>$yourName</h2>" ?>
+<h2>New Code Added </h2>
+<p>I now have an updated php file on my gitHub.</p>
 <body>
 </body>
 </html>
