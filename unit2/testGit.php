e0f4ed3a4a912617f55604156219002e33cf3b9e
Will Brown <wbrown.web@gmail.com>
2018-02-26 22:34:47 -0600

    New test folder uploaded to gitHub

---
+++ unit2/testGit.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>WDV341 Intro PHP - testGit</title>
+</head>
+  <?php $yourName = "Will Brown"; ?>
+  <?php echo "<h2>$yourName</h2>" ?>
+<body>
+</body>
+</html>
